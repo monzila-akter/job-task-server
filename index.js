a6f2d109ac56ce4c4d80d1bd1f2f1426897e48ec
monzila-akter <monzila01727@gmail.com>
2025-02-20 21:13:24 +0600

    get api

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const http = require("http"); // Required for WebSockets
+const { Server } = require("socket.io");
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
+
+// Create HTTP server for WebSockets
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vo9th.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,7 +37,20 @@ async function run() {
 
      const taskCollection = client.db("TaskDB").collection("tasks");
 
-     
+        //  **WebSocket Connection**
+    io.on("connection", (socket) => {
+        console.log("A user connected ");
+  
+        socket.on("disconnect", () => {
+          console.log("User disconnected ");
+        });
+      });
+
+    // GET - Retrieve Tasks**
+    app.get("/tasks", async (req, res) => {
+        const tasks = await taskCollection.find().toArray();
+        res.json(tasks);
+      });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
